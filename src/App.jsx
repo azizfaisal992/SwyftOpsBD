96ee24de2357cb5d70b7c1098bf8a99767f56637
azizfaisal992 <azizfaisal992@gmail.com>
2026-07-18 12:34:05 +0600

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import Footer from "./layout/footer";
+import Header from "./layout/header";
+import Home from "./pages/Home";
 
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const App = () => (
+  <div className="min-h-screen bg-white text-slate-900">
+    <Header />
+    <main>
+      <Home />
+    </main>
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
