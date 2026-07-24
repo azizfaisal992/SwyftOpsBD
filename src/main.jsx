c6ffc93e0acfaf29f3e841135e4af5f5e78011ec
azizfaisal992 <azizfaisal992@gmail.com>
2026-07-24 20:14:24 +0600

    udpated with path

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import AuthProvider from './context/AuthProvider.jsx'
+import CmsContentProvider from './context/CmsContentProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <CmsContentProvider>
+          <App />
+        </CmsContentProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
